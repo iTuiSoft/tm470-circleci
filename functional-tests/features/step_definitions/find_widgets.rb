3216f19346d0ed6ca6b6edc49182fd0a55cd719e
Christopher Hopkins <christopher.hopkins@dvla.gov.uk>
2020-06-19 15:58:53 +0100

    Add names to html and finish first test

--- functional-tests/features/step_definitions/find_widgets.rb
+++ functional-tests/features/step_definitions/find_widgets.rb
@@ -1,17 +1,28 @@
 require 'selenium-webdriver'
 
+driver = Selenium::WebDriver.for :chrome
+
 Given("I am on the Java home page") do
-  driver = Selenium::WebDriver.for :chrome
 
   driver.manage.window.maximize
 
   driver.get "http://localhost:8080/"
 end
 
-When("I search for Widget id {int}") do |int|
-  pending # Write code here that turns the phrase above into concrete actions
+When("I search for Widget id {int}") do |id|
+  widget_id_input = driver.find_element(:id, "widgetId")
+  widget_id_input.send_keys(id)
+  search_link = driver.find_element(:name, "search_link")
+  search_link.click
 end
 
-Then("widget id {int} is displayed") do |int|
-  pending # Write code here that turns the phrase above into concrete actions
+Then("widget id {int} is displayed") do |id|
+  widget_id = driver.find_element(:name, "id")
+
+  puts widget_id.text
+  if widget_id.text === id.to_s
+    puts "Test Passed"
+  else
+    raise "Test Failed"
+  end
 end
