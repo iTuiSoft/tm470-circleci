9fbb2111d7dd0bc8a6727058cbc47caef51d527f
Christopher Hopkins <christopher.hopkins@dvla.gov.uk>
2020-06-21 02:15:43 +0100

    Add element names for testing, complete cucumber tests, add environment variables for cucumber

--- functional-tests/features/step_definitions/find_widgets.rb
+++ functional-tests/features/step_definitions/find_widgets.rb
@@ -3,15 +3,12 @@ require 'selenium-webdriver'
 driver = Selenium::WebDriver.for :chrome
 
 Given("I am on the Java home page") do
-
-  driver.manage.window.maximize
-
-  driver.get "http://localhost:8080/"
+  driver.get Settings.base_url.java
 end
 
 When("I search for Widget id {int}") do |id|
-  widget_id_input = driver.find_element(:id, "widgetId")
-  widget_id_input.send_keys(id)
+  id_input = driver.find_element(:id, "widgetId")
+  id_input.send_keys(id)
   search_link = driver.find_element(:name, "search_link")
   search_link.click
 end
@@ -19,7 +16,6 @@ end
 Then("widget id {int} is displayed") do |id|
   widget_id = driver.find_element(:name, "id")
 
-  puts widget_id.text
   if widget_id.text === id.to_s
     puts "Test Passed"
   else
